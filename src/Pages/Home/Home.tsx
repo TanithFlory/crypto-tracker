26edefa600d4fb0c605cc9aafb5694fe694a972a
Tanith Flory <tanithflory7770@gmail.com>
2023-05-31 10:45:50 +0530

    .

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -2,12 +2,14 @@ import Header from "../../Components/Header/Header";
 import CryptoBenefits from "../../Components/CryptoBenefits/CryptoBenefits";
 import Socials from "../../Components/Socials/Socials";
 import Footer from "../../Components/Footer/Footer";
+import Coins from "../Coins/Coins";
 const Home = () => {
   return (
     <>
       <Header />
       <CryptoBenefits />
       <Socials />
+      <Coins />
       <Footer />
     </>
   );
